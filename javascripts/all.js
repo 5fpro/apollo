bc7dd204a99e9eaa39818039817146d9f20d0117
marsz <marsz330@gmail.com>
2016-11-04 05:00:57 +0800

    --skip-ci

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,4 @@
 // This is where it all goes :)
+if (window.location.protocol != 'https:' && window.location.port == '80') {
+  window.location = window.location.toString().replace(/^http:/, "https:");
+}
