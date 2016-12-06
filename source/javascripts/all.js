147f07bef96cbb8deb5f22ab50cff2b5c8fef6c1
marsz <marsz330@gmail.com>
2016-12-06 15:58:27 +0800

    fix staging ssl bug

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,6 @@
 // This is where it all goes :)
 if (window.location.protocol != 'https:' && window.location.port == '') {
-  window.location = window.location.toString().replace(/^http:/, "https:");
+  if(window.location.indexOf('staging') == -1 && window.location.indexOf('localhost') == -1) {
+    window.location = window.location.toString().replace(/^http:/, "https:");
+  }
 }
