316007ae533f02cbcb7e6f09833a005d0aef233e
marsz <marsz330@gmail.com>
2016-12-06 15:52:54 +0800

    fix

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,4 +1,4 @@
 // This is where it all goes :)
-if (window.location.protocol != 'https:' && window.location.port == '80') {
+if (window.location.protocol != 'https:' && window.location.port == '') {
   window.location = window.location.toString().replace(/^http:/, "https:");
 }
