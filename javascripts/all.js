b0f5f484926e7bad0dfec99c2326d090f7fa4068
marsz <marsz330@gmail.com>
2016-12-18 22:00:07 +0800

    fix js

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,6 @@
 // This is where it all goes :)
 if (window.location.protocol != 'https:' && window.location.port == '') {
-  if(window.location.indexOf('staging') == -1 && window.location.indexOf('localhost') == -1) {
+  if(window.location.host.indexOf('staging') == -1 && window.location.host.indexOf('localhost') == -1) {
     window.location = window.location.toString().replace(/^http:/, "https:");
   }
 }
