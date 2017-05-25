b60739e0713ee8260fbee4b9554677d357e3cb5b
Irene Chang <irene84111@gmail.com>
2017-05-25 16:34:28 +0800

    modified Project section & added photos

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,23 @@
 //= require jquery/dist/jquery.min
+//= require bootstrap-sass/assets/javascripts/bootstrap.min.js
 //= require force_https
+
+$('.carousel').carousel()
+
+$(function(){
+  var hidden_project = $('.prehidden');
+      
+  hidden_project.hide();
+  
+  $('#project-more').click(function(e){
+    e.preventDefault();
+    hidden_project.slideToggle(1500, 'linear');
+
+    if (this.innerHTML == "VIEW MORE"){
+      this.innerHTML = "VIEW LESS";
+    }
+    else {
+      this.innerHTML = "VIEW MORE"
+    };
+  });
+});
