3f6c11b2f8eed1751e846c58f4d42d704db64fb4
Irene Chang <irene84111@gmail.com>
2017-06-15 16:34:56 +0800

    Completed Cover Section / Added libraries / Modified JS

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,29 @@
 //= require jquery/dist/jquery.min
 //= require bootstrap-sass/assets/javascripts/bootstrap.min.js
+//= require jquery-backstretch/jquery.backstretch.min.js
+//= require jquery.easing/js/jquery.easing.min.js
 //= require force_https
 
-$('.carousel').carousel()
-
 $(function(){
+
+  // Enable Hero Image
+  $.backstretch("../images/hero-image.jpg");
+
+  // Enable Bootstrap Carousel
+  $('.carousel').carousel()
+
+  // Explore Button Effect
+  $('#explore').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1000, 'easeInOutExpo');
+      event.preventDefault();
+  });
+
+  // View More Projects
   var hidden_project = $('.prehidden');
-      
+
   hidden_project.hide();
   
   $('#project-more').click(function(e){
