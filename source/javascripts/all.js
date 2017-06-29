443ee7e506b17fea6787c7fa53b48d0ba81be6fa
Irene Chang <irene84111@gmail.com>
2017-06-29 19:07:59 +0800

    optimized Cover and Navbar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -12,25 +12,24 @@ $(function(){
   // Enable Bootstrap Carousel
   $('.carousel').carousel()
 
-
   // jQuery for page scrolling feature - requires jQuery Easing plugin
   $('a.page-scroll').bind('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: ($($anchor.attr('href')).offset().top - 69)
-      }, 1000, 'easeInOutExpo');
-      event.preventDefault();
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 69)
+    }, 1000, 'easeInOutExpo');
+    event.preventDefault();
   });
 
   // Highlight the top nav as scrolling occurs
   $('body').scrollspy({
-      target: '.navbar-fixed-top',
-      offset: 70
+    target: '.navbar-fixed-top',
+    offset: 70
   });
 
   // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function(){ 
-     $('.navbar-toggle:visible').click();
+    $('.navbar-toggle:visible').click();
   });
 
   // Offset for Main Navigation
@@ -42,11 +41,11 @@ $(function(){
 
   // Explore Button Effect
   $('#explore').bind('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1000, 'easeInOutExpo');
-      event.preventDefault();
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1000, 'easeInOutExpo');
+    event.preventDefault();
   });
 
   // View More Projects
